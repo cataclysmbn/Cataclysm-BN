e7ddd20420b3b0fd6e423344f3b33fa3e99c8e2d
WishDuck <github@johnrosshiggins.com>
2026-01-04 18:50:51 -0800

    how did that sneak through

--- src/item.cpp
+++ src/item.cpp
@@ -285,9 +285,6 @@ item::item( const itype *type, time_point turn, int qty ) : type( type ),
     components( new component_item_location( this ) ), bday( turn )
 {
     item_vars = type->item_vars;
-    if( item_vars.contains( "test" ) ) {
-        std::cout << "success!";
-    }
     corpse = has_flag( flag_CORPSE ) ? &mtype_id::NULL_ID().obj() : nullptr;
     item_counter = type->countdown_interval;
 
