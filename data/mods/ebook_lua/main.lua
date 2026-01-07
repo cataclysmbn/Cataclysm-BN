97cfd074e38efe58b54a78cb5f805483081955ee
Alec White Valentine <doncuentacuentos@hotmail.com>
2026-01-07 19:10:30 +0100

    Initial commit

--- data/mods/ebook_lua/main.lua
+++ data/mods/ebook_lua/main.lua
@@ -579,7 +579,7 @@ mod.ebook_ui = function(who, item, pos)
   uilist:desc_enabled(true)
   uilist:text(
     string.format(
-      locale.gettext("Welcome to e-book library!\nThis device currently holds %d book(s).\nNetwork sync unavilable."),
+      locale.gettext("Welcome to e-book library!\nThis device currently holds %d book(s).\nNetwork sync unavailable."),
       var_count
     )
   )
