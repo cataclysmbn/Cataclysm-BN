6f2a562a908e7c7389e7c17852f7e5a28eab76a8
WishDuck <github@johnrosshiggins.com>
2026-01-18 17:26:31 -0800

    actually add the progress

---
+++ src/catalua_mapgen.cpp
@@ -0,0 +1,20 @@
+#include "catalua_mapgen.h"
+#include "catalua.h"
+#include "catalua_impl.h"
+#include "init.h"
+#include "player.h"
+
+mapgen_function_lua::mapgen_function_lua( const std::string &func, int weight ) : mapgen_function( weight )
+{
+    sol::state &lua = DynamicDataLoader::get_instance().lua->lua;
+    sol::object ref = lua.globals()["game"]["mapgen_functions"][func];
+    if( ref.get_type() == sol::type::function ) {
+        auto luafunc = ref.as<sol::function>();
+        generate_func = std::move( luafunc );
+    }
+}
+
+void mapgen_function_lua::generate( mapgendata &dat )
+{
+    generate_func( )
+}
