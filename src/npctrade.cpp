de3487fe8f9b048af1cc1fa2955f3f942e10d629
Shtacks <zjohnson9225@gmail.com>
2026-01-31 09:42:33 -0500

    reverse condition to check for merchant before dropping invalid inventory this seems to be the intended behavior in the past will look at this in a separate PR if needed

--- src/npctrade.cpp
+++ src/npctrade.cpp
@@ -262,9 +262,7 @@ auto npc_trading::trade( npc &np, int cost, const std::string &deal ) -> bool
     //np.drop_items( np.weight_carried() - np.weight_capacity(),
     //               np.volume_carried() - np.volume_capacity() );
     // skip drop invalid inventory for merchants to avoid item dump on floor
-    if( !np.is_shopkeeper() ) {
-        np.drop_invalid_inventory();
-    }
+    np.drop_invalid_inventory();
 
     auto state = trade_state{};
     npc_trading::setup_trade_state( state, cost, np );
