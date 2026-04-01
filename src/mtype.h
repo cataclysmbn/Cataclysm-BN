661e62424c2bec4de3972ad63fdc4919abf1791e
shmakota <47482311+shmakota@users.noreply.github.com>
2026-03-31 22:23:28 -0500

    flying enemies flee vertically, use preferred Z levels

--- src/mtype.h
+++ src/mtype.h
@@ -314,6 +314,7 @@ struct mtype {
         int speed = 0;          /** e.g. human = 100 */
         int agro = 0;           /** chance will attack [-100,100] */
         int morale = 0;         /** initial morale level at spawn */
+        std::optional<int> preferred_z;
 
         // Number of hitpoints regenerated per turn.
         int regenerates = 0;
@@ -482,4 +483,3 @@ struct mtype {
 };
 
 mon_effect_data load_mon_effect_data( const JsonObject &e );
-
