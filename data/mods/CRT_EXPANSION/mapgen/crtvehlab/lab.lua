7630c742e31ef30f4f9b43937599bf9d85a56326
WishDuck <github@johnrosshiggins.com>
2026-04-16 14:00:14 -0700

    itloads!

---
+++ data/mods/CRT_EXPANSION/mapgen/crtvehlab/lab.lua
@@ -0,0 +1,25 @@
+
+local crt_lab_veh_main = {}
+
+local mains = { "crt_lab_veh_house", "crt_lab_veh_training", "crt_lab_veh_wind", "crt_lab_veh_hall" }
+
+local mains_weight = { 100, 50, 100, 250 }
+
+local total_weight = 500
+
+crt_lab_veh_main.draw = function(data, map)
+  local weight = gapi.rng( 0, total_weight )
+  local count = 0
+  local main
+  for i, add in ipairs(mains_weight) do
+    count = count + add
+    if count >= weight then
+      main = mains[i]
+      break
+    end
+  end
+  data:generate( main )
+end
+
+return crt_lab_veh_main
+
--- data/mods/CRT_EXPANSION/preload.lua
+++ data/mods/CRT_EXPANSION/preload.lua
@@ -5,4 +5,5 @@ mod.storage = storage
 
 game.mapgen_functions["crt_lab_veh_house"] = function(...) return mod.crt_lab_veh_house.draw(...) end
 game.mapgen_functions["crt_lab_veh_hall"] = function(...) return mod.crt_lab_veh_hall.draw(...) end
+game.mapgen_functions["crt_lab_veh_main"] = function(...) return mod.crt_lab_veh_main.draw(...) end
 
