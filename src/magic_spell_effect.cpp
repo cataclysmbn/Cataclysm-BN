5f5640ca3cfb4fd9f4781d34bcd17922afa68c93
Chaosvolt <chaosvolt@users.noreply.github.com>
2026-04-18 02:11:52 -0500

    Add thing

--- src/magic_spell_effect.cpp
+++ src/magic_spell_effect.cpp
@@ -1247,12 +1247,12 @@ void spell_effect::mutate( const spell &sp, Creature &caster, const tripoint &ta
                 // As with serum, test threshold both before and after granting mutation.
                 if( sp.has_flag( spell_flag::MUTATE_THRESH ) ) {
                     test_crossing_threshold( *guy, mutation_category_trait::get_category(
-                                                 mutation_category_id( sp.effect_data() ) ), 1 );
+                                                 mutation_category_id( sp.effect_data() ) ), std::max ( sp.accuracy(), 1 ) );
                 }
                 guy->mutate_category( mutation_category_id( sp.effect_data() ) );
                 if( sp.has_flag( spell_flag::MUTATE_THRESH ) ) {
                     test_crossing_threshold( *guy, mutation_category_trait::get_category(
-                                                 mutation_category_id( sp.effect_data() ) ), 1 );
+                                                 mutation_category_id( sp.effect_data() ) ), std::max ( sp.accuracy(), 1 ) );
                 }
             }
         }
