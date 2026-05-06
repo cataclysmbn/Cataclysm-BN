333f0b8d53d188de80ecb6033504f50ce6e0c7cd
dedmemdev <unkown3587@gmail.com>
2026-05-06 15:46:58 +0000

    add camo

--- src/character.cpp
+++ src/character.cpp
@@ -7210,6 +7210,14 @@ int Character::visibility( bool, int ) const
     if( ( g->u.movement_mode_is( CMM_CROUCH ) ) ) {
         stealth_modifier += crouching_bonus;
     };
+    map &here = get_map();
+    int const camo_modifier = 50;
+    if ( worn_with_flag( flag_NATURE_CAMO ) && here.has_flag( "PLOWABLE" ) ) {
+        stealth_modifier += camo_modifier;
+    }
+    else if ( worn_with_flag( flag_URBAN_CAMO ) && here.has_flag( "MINEABLE" ) ) {
+        stealth_modifier += camo_modifier;
+    }
     return clamp( 100 - stealth_modifier, 20, 160 );
 }
 
