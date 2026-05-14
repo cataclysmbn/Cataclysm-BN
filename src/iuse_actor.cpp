34e56fdc0ee0526132b4ce6cce08e85e4c216010
Chaosvolt <chaosvolt@users.noreply.github.com>
2026-05-13 22:03:27 -0500

    blep

--- src/iuse_actor.cpp
+++ src/iuse_actor.cpp
@@ -3410,7 +3410,7 @@ bool repair_item_actor::can_repair_target( player &pl, const item &fix,
         }
         return false;
     }
-    if( fix.count_by_charges() || fix.has_flag( flag_NO_REPAIR ) ) {
+    if( fix.has_flag( flag_NO_REPAIR ) ) {
         if( print_msg ) {
             pl.add_msg_if_player( m_info, _( "You cannot repair this type of item." ) );
         }
