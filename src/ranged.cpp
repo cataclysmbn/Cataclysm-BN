aeb75987440cd21ff075973f59cfdc48a658fa0d
Wishduck <github@johnrosshiggins.com>
2026-06-04 18:06:10 -0700

    modify according to reta

--- src/ranged.cpp
+++ src/ranged.cpp
@@ -1182,9 +1182,9 @@ static int calc_gun_volume( const item &gun )
     if( gun.ammo_data() ) {
         noise += gun.ammo_data()->ammo->loudness;
         // Speed of sound at sea level is around 343 meters per second.
-        if( gun.ammo_data()->ammo->speed > 342 ) {
-            noise = std::min( 120, noise );
-        }
+        // While it would be ideal to be based on speed of sound
+        // EVERYTHING flies faster then the speed of sound so using that to force loud sounds makes little sense in the current state of affairs
+        noise = std::min( 140, noise );
     }
     for( const auto mod : parent.gunmods() ) {
         noise += mod->type->gunmod->loudness;
