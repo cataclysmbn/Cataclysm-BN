2851b320c06190af14fbadb55fe0f3a8e6aad363
Wishduck <github@johnrosshiggins.com>
2026-06-04 19:08:10 -0700

    eepy

--- src/ranged.cpp
+++ src/ranged.cpp
@@ -1184,8 +1184,8 @@ static int calc_gun_volume( const item &gun )
         // Speed of sound at sea level is around 343 meters per second.
         // While it would be ideal to be based on speed of sound
         // EVERYTHING flies faster then the speed of sound so using that to force loud sounds makes little sense in the current state of affairs
-        // NOTE: If supersonic ever gets implented, use it here
-        noise = std::max( 160, noise );
+        // NOTE: If supersonic ever gets implented, use it here 
+        noise = std::min( 160, noise );
     }
     for( const auto mod : parent.gunmods() ) {
         noise += mod->type->gunmod->loudness;
