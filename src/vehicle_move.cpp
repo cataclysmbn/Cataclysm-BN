d94229ba47c6b0783ab738857e36ab8b60d880f7
aspolito <aspolito@gmail.com>
2026-06-08 17:33:13 -0700

    do it for crazy drivers too!

--- src/vehicle_move.cpp
+++ src/vehicle_move.cpp
@@ -1323,7 +1323,6 @@ void vehicle::pldrive( Character &driver, tripoint_rel_veh p )
             cruise_thrust( -p.y() * thr_amount );
         } else {
             thrust( -p.y() );
-            driver.moves -= action_time_scale::vehicle_control_cost( driver, 100 );
         }
     }
 
