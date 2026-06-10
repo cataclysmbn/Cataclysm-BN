4463d1841d39f875c802df6742c6b47ba8153ff4
Reisen Usagi <usagirei@users.noreply.github.com>
2026-06-10 03:00:07 -0300

    rename type

--- src/dynamic_atlas.cpp
+++ src/dynamic_atlas.cpp
@@ -137,7 +137,7 @@ struct null_texture_packer final : detail::texture_packer {
     };
 };
 
-auto dynamic_atlas::update_staging_area(staging& staging, const int width, const int height) const
+auto dynamic_atlas::update_staging_area(staging_area& staging, const int width, const int height) const
     -> std::tuple<SDL_Texture*, SDL_Surface*, SDL_Rect> {
     const auto r_width = round_up(width, hint_sprite_width);
     const auto r_height = round_up(height, hint_sprite_height);
