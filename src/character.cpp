025cfb47074c14924f9959006a302204e83c3715
Wishduck <github@johnrosshiggins.com>
2026-06-17 07:33:08 -0700

    enchantify night vision

--- src/character.cpp
+++ src/character.cpp
@@ -2023,7 +2023,6 @@ void Character::recalc_sight_limits()
         best_bonus_nv = std::max( best_bonus_nv, 10.0f );
     }
     if( worn_with_flag( flag_GNV_EFFECT ) ||
-        has_active_bionic( bio_night_vision ) ||
         has_effect_with_flag( flag_EFFECT_NIGHT_VISION ) ) {
         vision_mode_cache.set( NV_GOGGLES );
         best_bonus_nv = std::max( best_bonus_nv, 10.0f );
@@ -2031,6 +2030,11 @@ void Character::recalc_sight_limits()
     if( worn_with_flag( flag_GNVE_EFFECT ) ) {
         vision_mode_cache.set( ENV_GOGGLES );
         best_bonus_nv = std::max( best_bonus_nv, 18.0f );
+    }
+    int ench_vision = bonus_from_enchantments( 0, enchantment_value_id( "NIGHT_VISION" ) );
+    if( ench_vision > best_bonus_nv ) {
+        vision_mode_cache.set( NV_GOGGLES );
+        best_bonus_nv = ench_vision;
     }
     if( has_trait( trait_BIRD_EYE ) ) {
         vision_mode_cache.set( BIRD_EYE );
