581eb005b546101e36d8b47ce96e0fa22a9c9b65
SublunarSage <lambdacolwell@gmail.com>
2026-09-11 16:10:14 -0700

    chore: Apply clang-ci to make autofix happy.

--- src/weather/weather.h
+++ src/weather/weather.h
@@ -26,7 +26,8 @@
 ///@{
 //!< More aggressive cold effects.
 static constexpr int BODYTEMP_FREEZING = 500;
-//!< This value means frostbite occurs at the warmest temperature of 1C. If changed, the temp_conv calculation should be reexamined.
+//!< This value means frostbite occurs at the warmest temperature of 1C. If changed, the temp_conv
+//!< calculation should be reexamined.
 static constexpr int BODYTEMP_VERY_COLD = 2000;
 //!< Frostbite timer will not improve while below this point.
 static constexpr int BODYTEMP_COLD = 3500;
@@ -46,48 +47,45 @@ class map;
 struct trap;
 struct rl_vec2d;
 
-double precip_mm_per_hour( precip_class p );
-void handle_bulk_weather_field_decay( const weather_type_id &w, int turns );
-void handle_weather_effects( const weather_type_id &w, bool do_decay = true );
+double precip_mm_per_hour(precip_class p);
+void handle_bulk_weather_field_decay(const weather_type_id& w, int turns);
+void handle_weather_effects(const weather_type_id& w, bool do_decay = true);
 
 /**
  * Weather drawing tracking.
- * Used for redrawing the view coordinates overwritten by the previous frame's animation bits (raindrops, snowflakes, etc,) and to draw this frame's weather animation.
+ * Used for redrawing the view coordinates overwritten by the previous frame's animation bits
+ * (raindrops, snowflakes, etc,) and to draw this frame's weather animation.
  * @see game::get_player_input
  */
 struct weather_printable {
     //!< Weather type in use.
     weather_type_id wtype;
     //!< Coordinates targeted for droplets.
-    std::vector<std::pair<int, int> > vdrops;
+    std::vector<std::pair<int, int>> vdrops;
     //!< Color to draw glyph this animation frame.
     nc_color colGlyph;
     //!< Glyph to draw this animation frame.
     uint32_t cGlyph;
-    std::string get_symbol() const {
-        return utf32_to_utf8( cGlyph );
-    }
+    std::string get_symbol() const { return utf32_to_utf8(cGlyph); }
 };
 
 /**
  * Environmental effects and ramifications of weather.
  * Visibility range changes are done elsewhere.
  */
-namespace weather_effect
-{
-void thunder( int intensity );
-void lightning( int intensity );
-void effect( int intensity, time_duration duration, bodypart_str_id bp_id,
-             int effect_intensity,
-             const std::string &effect_id_str,
-             const std::string &effect_msg,
-             int effect_msg_frequency, int effect_msg_blocked_frequency, game_message_type message_type,
-             std::string precipitation_name, std::vector<std::tuple<std::string, int>> );
-void morale( int intensity, int bonus, int bonus_max, time_duration duration,
-             time_duration decay_start,
-             const std::string &morale_id_str, const std::string &morale_msg,
-             int morale_msg_frequency, game_message_type message_type );
-void wet_player( int amount );
+namespace weather_effect {
+void thunder(int intensity);
+void lightning(int intensity);
+void effect(
+    int intensity, time_duration duration, bodypart_str_id bp_id, int effect_intensity,
+    const std::string& effect_id_str, const std::string& effect_msg, int effect_msg_frequency,
+    int effect_msg_blocked_frequency, game_message_type message_type,
+    std::string precipitation_name, std::vector<std::tuple<std::string, int>>);
+void morale(
+    int intensity, int bonus, int bonus_max, time_duration duration, time_duration decay_start,
+    const std::string& morale_id_str, const std::string& morale_msg, int morale_msg_frequency,
+    game_message_type message_type);
+void wet_player(int amount);
 } // namespace weather_effect
 
 struct weather_sum {
@@ -97,17 +95,16 @@ struct weather_sum {
     int wind_amount = 0;
 };
 
-namespace weather
-{
-bool is_sheltered( const map &m, const tripoint_bub_ms &p );
-bool is_in_sunlight( const map &m, const tripoint_bub_ms &p, const weather_type_id &weather );
+namespace weather {
+bool is_sheltered(const map& m, const tripoint_bub_ms& p);
+bool is_in_sunlight(const map& m, const tripoint_bub_ms& p, const weather_type_id& weather);
 } // namespace weather
 
-std::string get_shortdirstring( int angle );
+std::string get_shortdirstring(int angle);
 
-std::string get_dirstring( int angle );
+std::string get_dirstring(int angle);
 
-std::string weather_forecast( const point_abs_sm &abs_sm_pos );
+std::string weather_forecast(const point_abs_sm& abs_sm_pos);
 
 // Returns input value (in Fahrenheit) converted to whatever temperature scale set in options.
 //
@@ -115,21 +112,20 @@ std::string weather_forecast( const point_abs_sm &abs_sm_pos );
 // If scale is Fahrenheit: temperature(100) will return "100F"
 //
 // Use the decimals parameter to set number of decimal places returned in string.
-std::string print_temperature( double fahrenheit, int decimals = 0 );
-std::string print_temperature( units::temperature temperature, int decimals = 0 );
-std::string print_humidity( double humidity, int decimals = 0 );
-std::string print_pressure( double pressure, int decimals = 0 );
+std::string print_temperature(double fahrenheit, int decimals = 0);
+std::string print_temperature(units::temperature temperature, int decimals = 0);
+std::string print_humidity(double humidity, int decimals = 0);
+std::string print_pressure(double pressure, int decimals = 0);
 
 // Return windchill offset in degrees F, starting from given temperature, humidity and wind
-int get_local_windchill( double temperature_f, double humidity, double wind_mph );
+int get_local_windchill(double temperature_f, double humidity, double wind_mph);
 
-int get_local_humidity( double humidity, const weather_type_id &weather, bool sheltered = false );
-double get_local_windpower( double windpower, const oter_id &omter, const tripoint_abs_ms &location,
-                            const int &winddirection,
-                            bool sheltered = false );
-weather_sum sum_conditions( const time_point &start,
-                            const time_point &end,
-                            const tripoint_abs_ms &location );
+int get_local_humidity(double humidity, const weather_type_id& weather, bool sheltered = false);
+double get_local_windpower(
+    double windpower, const oter_id& omter, const tripoint_abs_ms& location,
+    const int& winddirection, bool sheltered = false);
+weather_sum sum_conditions(
+    const time_point& start, const time_point& end, const tripoint_abs_ms& location);
 
 /**
  * @param it The container item which is to be filled.
@@ -137,22 +133,23 @@ weather_sum sum_conditions( const time_point &start,
  * by the @ref map, but absolute).
  * @param tr The funnel (trap which acts as a funnel).
  */
-void retroactively_fill_from_funnel( item &it, const trap &tr, const time_point &start,
-                                     const time_point &end, const tripoint_abs_ms &pos );
+void retroactively_fill_from_funnel(
+    item& it, const trap& tr, const time_point& start, const time_point& end,
+    const tripoint_abs_ms& pos);
 
-double funnel_charges_per_turn( double surface_area_mm2, double rain_depth_mm_per_hour );
+double funnel_charges_per_turn(double surface_area_mm2, double rain_depth_mm_per_hour);
 
-rl_vec2d convert_wind_to_coord( int angle );
+rl_vec2d convert_wind_to_coord(int angle);
 
-std::string get_wind_arrow( int );
+std::string get_wind_arrow(int);
 
-std::string get_wind_desc( double );
+std::string get_wind_desc(double);
 
-nc_color get_wind_color( double );
+nc_color get_wind_color(double);
 /**
-* Calculates rot per hour at given temperature. Reference in weather_data.cpp
-*/
-auto get_hourly_rotpoints_at_temp( const units::temperature temp ) -> int;
+ * Calculates rot per hour at given temperature. Reference in weather_data.cpp
+ */
+auto get_hourly_rotpoints_at_temp(const units::temperature temp) -> int;
 
 /**
  * Is it warm enough to plant seeds?
@@ -160,81 +157,73 @@ auto get_hourly_rotpoints_at_temp( const units::temperature temp ) -> int;
  * The first overload is in map-square coords, the second for larger scale
  * queries.
  */
-bool warm_enough_to_plant( const tripoint_abs_ms &pos );
-bool warm_enough_to_plant( const tripoint_abs_omt &pos );
+bool warm_enough_to_plant(const tripoint_abs_ms& pos);
+bool warm_enough_to_plant(const tripoint_abs_omt& pos);
 
-bool is_wind_blocker( const tripoint_bub_ms &location );
+bool is_wind_blocker(const tripoint_bub_ms& location);
 
-const weather_type_id &current_weather( const tripoint_abs_ms &location,
-                                        const time_point &t = calendar::turn );
+const weather_type_id& current_weather(
+    const tripoint_abs_ms& location, const time_point& t = calendar::turn);
 /**
  * Glare.
  * Causes glare effect to player's eyes if they are not wearing applicable eye protection.
  * @param intensity Level of sun brighthess
  */
-void glare( const weather_type_id &w );
+void glare(const weather_type_id& w);
 
 /**
  * Amount of sunlight incident at the ground, taking weather and time of day
  * into account.
  */
-int incident_sunlight( const weather_type_id &wtype,
-                       const time_point &t = calendar::turn );
+int incident_sunlight(const weather_type_id& wtype, const time_point& t = calendar::turn);
 
-class weather_manager
-{
-    public:
-        weather_manager();
-        ~weather_manager();
+class weather_manager {
+public:
+    weather_manager();
+    ~weather_manager();
 
-        const weather_generator &get_cur_weather_gen() const;
+    const weather_generator& get_cur_weather_gen() const;
 
-        // Updates the temperature and weather pattern
-        void update_weather();
-        // The air temperature
-        units::temperature temperature = 0_c;
-        units::temperature water_temperature = 0_c;
-        bool lightning_active = false;
-        // Weather pattern
-        weather_type_id weather_id = weather_type_id::NULL_ID();
-        int winddirection = 0;
-        int windspeed = 0;
+    // Updates the temperature and weather pattern
+    void update_weather();
+    // The air temperature
+    units::temperature temperature = 0_c;
+    units::temperature water_temperature = 0_c;
+    bool lightning_active = false;
+    // Weather pattern
+    weather_type_id weather_id = weather_type_id::NULL_ID();
+    int winddirection = 0;
+    int windspeed = 0;
 
-        std::optional<int> wind_direction_override;
-        std::optional<int> windspeed_override;
-        weather_type_id weather_override;
-        bool eternal_seasons = false;
+    std::optional<int> wind_direction_override;
+    std::optional<int> windspeed_override;
+    weather_type_id weather_override;
+    bool eternal_seasons = false;
 
-        // not only sets nextweather, but updates weather as well
-        void set_nextweather( time_point t );
-        // The time at which weather will shift next.
-        time_point nextweather;
+    // not only sets nextweather, but updates weather as well
+    void set_nextweather(time_point t);
+    // The time at which weather will shift next.
+    time_point nextweather;
 
-        /** temperature cache, cleared every turn, sparse map of map tripoints to temperatures */
-        mutable std::unordered_map< tripoint_abs_ms, units::temperature > temperature_cache;
-        // Returns outdoor or indoor temperature of given location (in local coords).
-        auto get_temperature( const tripoint_abs_ms &location ) const -> units::temperature;
-        // Returns outdoor or indoor temperature of given location
-        auto get_temperature( const tripoint_abs_omt &location ) const -> units::temperature;
-        // Returns water temperature of given location (in local coords).
-        auto get_water_temperature( const tripoint_abs_ms &location ) const -> units::temperature;
-        void clear_temp_cache();
+    /** temperature cache, cleared every turn, sparse map of map tripoints to temperatures */
+    mutable std::unordered_map<tripoint_abs_ms, units::temperature> temperature_cache;
+    // Returns outdoor or indoor temperature of given location (in local coords).
+    auto get_temperature(const tripoint_abs_ms& location) const -> units::temperature;
+    // Returns outdoor or indoor temperature of given location
+    auto get_temperature(const tripoint_abs_omt& location) const -> units::temperature;
+    // Returns water temperature of given location (in local coords).
+    auto get_water_temperature(const tripoint_abs_ms& location) const -> units::temperature;
+    void clear_temp_cache();
 
-        // Get precise weather data
-        const w_point &get_precise() const {
-            return weather_precise;
-        }
+    // Get precise weather data
+    const w_point& get_precise() const { return weather_precise; }
 
-        // For use in tests
-        void override_humidity( int h ) {
-            weather_precise.humidity = h;
-        }
+    // For use in tests
+    void override_humidity(int h) { weather_precise.humidity = h; }
 
-    private:
-        // Cached weather data
-        w_point weather_precise;
+private:
+    // Cached weather data
+    w_point weather_precise;
 };
 
-weather_manager &get_weather();
-
-
+weather_manager& get_weather();
--- src/weather/weather_gen.h
+++ src/weather/weather_gen.h
@@ -25,56 +25,56 @@ struct season_modifier {
     int humidity_mod = 0;
 };
 
-class weather_generator
-{
-    public:
-        // Average humidity
-        double base_humidity = 0;
-        // Average atmospheric pressure
-        double base_pressure = 0;
-        double base_acid = 0;
-        //Average yearly windspeed
-        double base_wind = 0;
-        //How much the wind peaks above average
-        int base_wind_distrib_peaks = 0;
-        std::array<season_modifier, 4> season_stats;
-        //How much the wind folows seasonal variation ( lower means more change )
-        int base_wind_season_variation = 0;
+class weather_generator {
+public:
+    // Average humidity
+    double base_humidity = 0;
+    // Average atmospheric pressure
+    double base_pressure = 0;
+    double base_acid = 0;
+    // Average yearly windspeed
+    double base_wind = 0;
+    // How much the wind peaks above average
+    int base_wind_distrib_peaks = 0;
+    std::array<season_modifier, 4> season_stats;
+    // How much the wind folows seasonal variation ( lower means more change )
+    int base_wind_season_variation = 0;
 
-        // Half the difference between coldest hour and warmest hour
-        units::temperature temperature_daily_amplitude = 5_c;
-        // Half the difference between coldest point in 3D coords and warmest one
-        units::temperature temperature_noise_amplitude = 8_c;
+    // Half the difference between coldest hour and warmest hour
+    units::temperature temperature_daily_amplitude = 5_c;
+    // Half the difference between coldest point in 3D coords and warmest one
+    units::temperature temperature_noise_amplitude = 8_c;
 
-        // TODO: Remove this horrible static variable!
-        static int current_winddir;
-        std::vector<weather_type_id> weather_types;
-        weather_generator();
+    // TODO: Remove this horrible static variable!
+    static int current_winddir;
+    std::vector<weather_type_id> weather_types;
+    weather_generator();
 
-        const weather_type_id &get_bad_weather() const;
-        const weather_type_id &get_default_weather() const;
+    const weather_type_id& get_bad_weather() const;
+    const weather_type_id& get_default_weather() const;
 
-        int forecast_priority( const weather_type_id &w ) const;
+    int forecast_priority(const weather_type_id& w) const;
 
-        /**
-         * TODO: Remove the regular tripoint overload, replace with *_abs_ms one.
-         */
-        w_point get_weather( const tripoint_abs_ms &, const time_point &, unsigned seed ) const;
-        w_point get_weather( const tripoint_abs_ms &location, const time_point &t,
-                             const calendar_config &calendar_config, unsigned seed ) const;
-        const weather_type_id &get_weather_conditions( const tripoint_abs_ms &, const time_point &,
-                unsigned seed ) const;
-        const weather_type_id &get_weather_conditions( const w_point & ) const;
-        int get_wind_direction( season_type ) const;
-        int convert_winddir( int ) const;
-        void test_weather( unsigned ) const;
+    /**
+     * TODO: Remove the regular tripoint overload, replace with *_abs_ms one.
+     */
+    w_point get_weather(const tripoint_abs_ms&, const time_point&, unsigned seed) const;
+    w_point get_weather(
+        const tripoint_abs_ms& location, const time_point& t,
+        const calendar_config& calendar_config, unsigned seed) const;
+    const weather_type_id& get_weather_conditions(
+        const tripoint_abs_ms&, const time_point&, unsigned seed) const;
+    const weather_type_id& get_weather_conditions(const w_point&) const;
+    int get_wind_direction(season_type) const;
+    int convert_winddir(int) const;
+    void test_weather(unsigned) const;
 
-        units::temperature get_weather_temperature( const tripoint_abs_ms &, const time_point &,
-                const calendar_config &calendar_config, unsigned ) const;
-        units::temperature get_water_temperature( const tripoint_abs_ms &, const time_point &,
-                const calendar_config &calendar_config, unsigned ) const;
+    units::temperature get_weather_temperature(
+        const tripoint_abs_ms&, const time_point&, const calendar_config& calendar_config,
+        unsigned) const;
+    units::temperature get_water_temperature(
+        const tripoint_abs_ms&, const time_point&, const calendar_config& calendar_config,
+        unsigned) const;
 
-        static weather_generator load( const JsonObject &jo );
+    static weather_generator load(const JsonObject& jo);
 };
-
-
