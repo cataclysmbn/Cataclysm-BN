6bc4904c9aee034a59da4ed1cdd6fdd41a2cea80
dedmemdev <unkown3587@gmail.com>
2026-09-15 03:19:41 +0000

    Update monmove.cpp

--- src/monmove.cpp
+++ src/monmove.cpp
@@ -2505,7 +2505,7 @@ bool monster::move_to( const tripoint_bub_ms &p, bool force, bool step_on_critte
                                                        destination ) : calc_movecost( bub_pos(),
                                                                destination ) );
         if( cost > 0.0f ) {
-            moves -= static_cast<int>( std::ceil( cost ) );;
+            moves -= static_cast<int>( std::ceil( cost ) );
         } else {
             return false;
         }
